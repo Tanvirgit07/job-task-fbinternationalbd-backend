faa08787000d405d1ddc4a081a8264d30b3eccc1
Tanvirgit07 <ahmmedtanvir009@gmail.com>
2026-01-24 20:32:30 +0600

    done user schema

---
+++ src/models/user.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: [true, "Username is required"],
+      unique: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+    },
+    role: {
+      type: String,
+      enum: ["admin", "inspector", "viewer"],
+      default: "viewer",
+    },
+  },
+  {
+    timestamps: true, 
+  }
+);
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
